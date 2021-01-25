b2f95dc83e2ebbeeeaa00eafd7dd3541120278ee
alnarozhny <alnarozhny@gmail.com>
2021-01-25 15:42:54 +0200

    remove all

--- Examples/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-//define('API_KEY', '1a3c2fa6735f089a2a1dd4fa11067807383bd08c');
-define('API_KEY', '112dec343c6dc9fe9517d40b728fb84724cd44a0');
-define('API_SECRET', '5a0ae446a98a1');
-define('API_ENDPOINT', 'http://tools.local-brightlocal.com/seo-tools/api'); // optional
-//
-//define('API_KEY', '<API_KEY_HERE>');
-//define('API_SECRET', '<API_SECRET_HERE>');
-//define('API_ENDPOINT', null); // optional
