cdf3293106c865279e34fb895021a32eb5131745
alnarozhny <alnarozhny@gmail.com>
2021-02-03 11:11:13 +0200

    Add examples: Google My Business - run report

--- Examples/GoogleMyBusiness/RunReport.php
+++ Examples/GoogleMyBusiness/RunReport.php
@@ -3,10 +3,9 @@ require '../../vendor/autoload.php';
 
 use BrightLocal\Api;
 
-$reportId = 1;
 $api = new Api('<YOUR_API_KEY>', '<YOUR_API_SECRET>');
 $resource = '/v4/gpw/run';
-$response = $api->put($resource . $reportId, [
+$response = $api->post($resource, [
     'report-id' => 860
 ]);
 print_r($response->getResult());
